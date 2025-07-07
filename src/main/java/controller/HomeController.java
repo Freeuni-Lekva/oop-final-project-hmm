3dabc74e8f9037b224502dd8d3ff8b9e5a2932d0
NikaSadghobelashvili <nika.sadgobelashvili09@gmail.com>
2025-07-07 23:24:02 +0400

    adding homeController servlet for controlling redirection to main page, also adding updated dependencies file

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,4 +1,35 @@
 package controller;
 
-public class HomeController {
+import dao.QuizDAO;
+import model.Quiz;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet(urlPatterns = {"", "/"})
+public class HomeController extends HttpServlet {
+    private QuizDAO quizDAO;
+
+    @Override
+    public void init() throws ServletException {
+        Connection connection = (Connection) getServletContext().getAttribute("DBConnection");
+        quizDAO = (QuizDAO) getServletContext().getAttribute("quizDAO");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            List<Quiz> quizzes = quizDAO.getAllQuizzes();
+            req.setAttribute("quizzes", quizzes);
+            req.getRequestDispatcher("/index.jsp").forward(req, resp);
+        } catch (SQLException e) {
+            throw new ServletException(e);
+        }
+    }
 }
